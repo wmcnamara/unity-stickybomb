b0fc3f248fd0d01241beb888ca41f07b1970c5ee
Weston Mcnamara <westonfmcnamara@gmail.com>
2022-01-14 11:43:31 -0500

    added tooltips, null checks and improved directory

--- Assets/StickyBomb/StickyBomb.cs
+++ Assets/StickyBomb/StickyBomb.cs
@@ -3,13 +3,31 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
 public class StickyBomb : MonoBehaviour
 {
-    [SerializeField] private GameObject explosionEffect;
-    [SerializeField] private AudioClip explosionSound;
-    [SerializeField] private float explosionVolume = 1.0f;
+    [Header("FX")]
 
+    [Tooltip("Optional explosion effect that will be spawned on the bomb on detonation. Leave empty for no effect.")]
+    [SerializeField] private GameObject explosionEffect;
+
+    [Tooltip("Optional explosion audio clip that will be played when the bomb detonates. Leave empty for no sound.")]
+    [SerializeField] private AudioClip explosionSound;
+
+    [Tooltip("The volume scale of the explosion sound")]
+    [SerializeField] [Range(0.0f, 1.0f)] private float explosionVolume = 1.0f;
+
+
+
+    [Header("Explosion Config")]
+
+    [Tooltip("The force of the explosion (which may be modified by distance).")]
     [SerializeField] private float explosionForce = 10.0f;
+
+    [Tooltip("The radius of the sphere within which the explosion has its effect.")]
     [SerializeField] private float explosionRadius = 10.0f;
-    [SerializeField] private float explosionDelay = 1.5f;
+
+    [Tooltip("A delay measured in seconds, that the bomb will wait after sticking to a surface before detonating.")]
+    [SerializeField] private float explosionDelay = 1f;
+
+    [Tooltip("Adjustment to the apparent position of the explosion to make it seem to lift objects.")]
     [SerializeField] private float explosionUpwardForce = 1.0f;
 
     private Rigidbody rb;
@@ -22,11 +40,22 @@ public class StickyBomb : MonoBehaviour
 
     public void Explode()
     {
-        Debug.Log("Exploded!");
+        /*
+            Spawn in explosion effect, and sound effect if the user has configured them in the inspector. 
+        */
+        if (explosionEffect != null)
+        {
+            var effect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
 
-        var effect = Instantiate(explosionEffect, transform.position, Quaternion.identity);
-        effect.AddComponent<AudioSource>().PlayOneShot(explosionSound, explosionVolume);
+            if (explosionSound != null)
+            {
+                effect.AddComponent<AudioSource>().PlayOneShot(explosionSound, explosionVolume);
+            }
+        }
 
+        /*
+            Get every collider in the radius near the bomb, and apply an explosion force to each. 
+        */
         Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
         foreach (Collider hit in colliders)
         {
